5cd2b8b2596f84d243397d0aa0156a1757073bd5
Afsar <mohammedafsar@propertyguru.com.sg>
2023-07-03 00:38:12 +0800

    change directory

--- Scripts/RunTest.sh
+++ Scripts/RunTest.sh
@@ -5,13 +5,12 @@ ls -lrt
 
 
 rm -rf node_modules
-npm i -g npm-upgrade
-npm i y18n
+# npm i -g npm-upgrade
+# npm i y18n
 
 npm install -g @wdio/cli
-# npm i appium-uiautomator2-driver
 
 npm install
-wdio run ./wdio.conf.js
+# wdio run ./wdio.conf.js
 # npm install
-# npm run wdio
+ npm run wdio
