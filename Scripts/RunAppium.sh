6cbfc1ce84dd0e7f6b1e3324e4f4ec43e4721102
Afsar <mohammedafsar@propertyguru.com.sg>
2023-07-03 00:11:14 +0800

    change directory

--- Scripts/RunAppium.sh
+++ Scripts/RunAppium.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
-npm install -g appium@next
+npm install -g appium
 # appium driver install xcuitest
 # appium driver install espresso
 
 appium -v
-appium driver install uiautomator2@2.0.1
+appium driver install uiautomator2
 appium --base-path /wd/hub --log appium.log &>/dev/null &
